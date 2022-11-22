3d61cb8f17f7a6b87e7af88a9f03975900a7eea2
Jere <jere.salminen@gritlab.ax>
2022-11-22 02:59:16 +0200

    sqllite

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"log"
+	"mycrud/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	err := models.ConnectDatabase()
+	checkErr(err)
+
+	r := gin.Default()
+
+	// API v1
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("person", getPersons)
+		v1.GET("person/:id", getPersonById)
+		v1.POST("person", addPerson)
+		v1.PUT("person/:id", updatePerson)
+		v1.DELETE("person/:id", deletePerson)
+		v1.OPTIONS("person", options)
+	}
+
+	// By default it serves on :8080 unless a
+	// PORT environment variable was defined.
+	r.Run()
+}
+
+func getPersons(c *gin.Context) {
+
+	persons, err := models.GetPersons(10)
+	checkErr(err)
+
+	if persons == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No Records Found"})
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{"data": persons})
+	}
+}
+
+func getPersonById(c *gin.Context) {
+
+	id := c.Param("id")
+
+	person, err := models.GetPersonById(id)
+	checkErr(err)
+	// if the name is blank we can assume nothing is found
+	if person.FirstName == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No Records Found"})
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{"data": person})
+	}
+}
+
+func addPerson(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "addPerson Called"})
+}
+
+func updatePerson(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "updatePerson Called"})
+}
+
+func deletePerson(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "deletePerson " + id + " Called"})
+}
+
+func options(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "options Called"})
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
